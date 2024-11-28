795a64ddc72e8d66f2c2ae72f510846861ba36d0
Dominique Vienne <dominique.vienne@ik.me>
2024-11-28 15:47:34 +0100

    Removed unicity on name

---
+++ database/migrations/2024_11_27_231243_update_games_table_remove_name_unique.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->dropUnique('games_name_unique');
+            $table->index('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->dropIndex('games_name_index');
+            $table->unique('games_name_unique');
+        });
+    }
+};
