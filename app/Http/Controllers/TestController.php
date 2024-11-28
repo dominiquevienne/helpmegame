c257441fb3a2fc4c82adcd44e7e2e285bcb5af54
Dominique Vienne <dominique.vienne@ik.me>
2024-11-28 15:47:03 +0100

    Disabling logic in GameController

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
 use App\Jobs\ProcessGameFetch;
+use App\Models\Game;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 
@@ -14,10 +16,5 @@ class TestController extends Controller
 {
     public function index(Request $request)
     {
-        $i = 10501;
-        while($i <= 12000) {
-            dispatch(new ProcessGameFetch($i));
-            $i++;
-        }
     }
 }
